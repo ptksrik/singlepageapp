004ddefa75edf6f9b652ee487e1105369f526d84
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-02-02 13:01:38 +0200

    Sivupohja

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './fragments/Navbar';
+import Header from './fragments/Header';
+import Footer from './fragments/Footer';
+import Home from './Home';
+import About from './About';
+import ContactUs from './Contact';
+import Notfound from './Notfound';
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
+  console.log('test')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Header />
+      <div className="container">
+
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={ContactUs} />
+          <Route path="/notfound" component={Notfound} />
+        </Switch>
+
+      </div>
+
+      <Footer />
+    </>
   );
 }
 
---
+++ src/fragments/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <span className="text-muted">&copy; Riku Pitkänen</span>
+            </div>
+        </footer>
+    )
+}
